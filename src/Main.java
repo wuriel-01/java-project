bcb94a986eb3f0c8f1d3339cacc256160545551d
wuriel-01 <bnarrdo@gmail.com>
2026-08-23 13:47:00 -0300

    CODIGO FUENTE

--- src/Main.java
+++ src/Main.java
@@ -1,91 +1,53 @@
 import javax.swing.*;
-import java.util.ArrayList;
 import java.awt.*;
 
 //PROYECTO ALUMNO: CASTILLO URIEL
 
 void main() {
 
-    //COMPONENTES
-    JFrame ventana = new JFrame("\uD83C\uDF93 GESTIÓN DE CURSOS");
+    JFrame ventana = new JFrame("Programa en VS Code");
+    ventana.setSize(350, 150);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ventana.setLocationRelativeTo(null);
 
-    JPanel panelSuperior= new JPanel();
-    JPanel panelInferior= new JPanel();
+    JPanel panel = new JPanel();
 
-    JButton botonAgregarCurso= new JButton("agregar curso");
+    JLabel etiqueta = new JLabel("Tu nombre:");
+    JTextField cajaTexto = new JTextField(15);
 
-    JButton botonEliminar= new JButton("Eliminar");
-    JButton botonBuscar= new JButton("Buscar");
-    JButton botonLimpiar= new JButton("Limpiar");
+    JLabel labelMensaje = new JLabel("Deja tu mensaje");
+    JTextField cajaMensaje= new JTextField(15);
 
-    JLabel labelCursosAgregados= new JLabel("Cursos Agregados");
+    JTextArea areaMensajes = new JTextArea();
 
-    JLabel labelNombre= new JLabel("Nombre");
-    JTextField ingresarNombre= new JTextField(15);
-
-    JLabel labelLenguaje= new JLabel("Lenguaje");
-    JTextField ingresarLenguaje= new JTextField(15);
-
-    JLabel labelDescripcion= new JLabel("Descripcion");
-    JTextField ingresarDescripcion= new JTextField(15);
-
-    JTextArea areaCursos = new JTextArea();
+    JButton boton = new JButton("Saludar");
+    JButton botonAgregarMensaje= new JButton("Agrega un mensaje");
 
 
-    //PANEL SUPERIOR
-    panelSuperior.add(labelNombre);
-    panelSuperior.add(ingresarNombre);
+    boton.addActionListener(e -> {
+        String texto = cajaTexto.getText();
+        JOptionPane.showMessageDialog(ventana, "¡Hola, " + texto + "!");
+    });
 
-    panelSuperior.add(labelLenguaje);
-    panelSuperior.add(ingresarLenguaje);
-
-    panelSuperior.add(labelDescripcion);
-    panelSuperior.add(ingresarDescripcion);
-
-    panelSuperior.add(botonAgregarCurso);
-    panelSuperior.setLayout(new GridLayout(4, 2));
-
-    //PANEL INFERIOR
-    panelInferior.add(labelCursosAgregados);
-    panelInferior.add(areaCursos);
-    panelInferior.add(botonEliminar);
-    panelInferior.add(botonBuscar);
-    panelInferior.add(botonLimpiar);
-    panelInferior.setLayout(new FlowLayout());
-
-    //FUNCIONALIDADES
-    ArrayList<Curso> cursos = new ArrayList<>();
-    botonAgregarCurso.addActionListener(e -> {
-
-        String nombre = ingresarNombre.getText();
-        String lenguaje = ingresarLenguaje.getText();
-        String descripcion = ingresarDescripcion.getText();
-
-        Curso nuevoCurso = new Curso(
-                nombre,
-                lenguaje,
-                descripcion
-        );
-
-        cursos.add(nuevoCurso);
-
-        areaCursos.append(
-                "Nombre: " + nuevoCurso.getNombre() + "\n"
+    botonAgregarMensaje.addActionListener(e->{
+        String mensaje=cajaMensaje.getText();
+        areaMensajes.append(
+                "Mensaje: " + mensaje + "\n"
         );
     });
 
+    panel.add(etiqueta);
+    panel.add(cajaTexto);
+    panel.add(boton);
+    panel.add(labelMensaje);
+    panel.add(cajaMensaje);
+    panel.add(areaMensajes);
+    panel.add(botonAgregarMensaje);
 
 
-    //VENTANA
-    ventana.setSize(500, 600);
-    ventana.setLayout(new GridLayout(2, 1));
-
-    ventana.add(panelSuperior);
-    ventana.add(panelInferior);
-
-    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    ventana.setLocationRelativeTo(null);
+    ventana.add(panel);
     ventana.setVisible(true);
+
 
 
 }
